89403d2631706d742a70c6c818c0585aaa0630c7
A-nirvana <anirban_ug_23@gmail.com>
2024-09-06 00:06:45 +0530

    fixed .env issues

--- server.ts
+++ server.ts
@@ -1,16 +1,18 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors'
 import { applicantRouter } from './src/routes';
-
+import dotenv from 'dotenv'
+import path from 'path';
 
 const app  : express.Application = express()
+dotenv.config({ path: path.join(__dirname, `./.env`) });
 
 app.use(express.json())
 app.use(cors())
 
 app.get("/", async(req:Request, res: Response)=>{
     res.json({
-        message : "hello world"
+        message : process.env.EMAIL_MAIN || "undf"
     })
 })
 
