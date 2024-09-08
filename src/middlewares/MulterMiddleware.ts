36d8497f8fd76e25934d1ce925006d4729ed6d23
yuvrajkAkati <139755961+yuvrajkAkati@users.noreply.github.com>
2024-09-08 12:18:23 +0530

    env.example added and prisma schema changed (#8)
    yuvi done

--- src/middlewares/MulterMiddleware.ts
+++ src/middlewares/MulterMiddleware.ts
@@ -1,8 +1,8 @@
 import multer from 'multer'
-
+const id : number = 2;
 const storage = multer.diskStorage({
     destination : function(req,file,cb){
-        cb(null,'./temp/ApplicantData')
+        cb(null,`./temp/Applciant_dataNow_${id}`)
     },
     filename : function(req,file,cb){
         cb(null,file.originalname)
@@ -10,4 +10,3 @@ const storage = multer.diskStorage({
 })
 
 export const upload = multer({storage : storage})
-
---
+++ src/routes/Admin.ts
@@ -0,0 +1,10 @@
+
+
+// //admin routes
+
+// admin api endpoints (signin,signup) 
+// a)total money required +- 12312 // will fetch data from the database make a schema for all the available money,seed data 
+// b)total funds // more or less same 
+// c)list of students who have applied  // fetch data from the user (for now make it applicant later) 
+// d)students with specific id and also return his progress // {id of the student as query_params}
+// e)can verify a students document , mark it true or false // { id of the student and typeof certificate as query params } {}
